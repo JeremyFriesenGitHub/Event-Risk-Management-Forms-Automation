b024b2bee0f4b047dcf40e9f563d217bf8130b77
JeremyFriesenGitHub <jeremyfriesen12@gmail.com>
2024-11-11 20:52:39 -0500

    chore: add cli workflow for all inputs

--- src/input/hybrid-input.js
+++ src/input/hybrid-input.js
@@ -36,14 +36,35 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
 var hybrid_1 = require("../hybrid");
-(function () { return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, (0, hybrid_1.scheduleHybrid)('Raef', 'Sarofiem', '101266475', 'raefsarofiem@cmail.carleton.ca', 'RAEF_PHONE_NUMBER', 'Ajaan', 'Nalliah', '101325463', 'ajaannalliah@cmail.carleton.ca', '6478773250', 'INPUT_FROM_HERE', '2024/10/23', '10:00 AM', '12:00 PM', 'Event Description', 'Event Location', '100', 'Zoom', 'Technology', 'Online', '5', '2', 'INPUT_UNTIL_HERE', 'Raef Sarofiem, Ajaan Nalliah', 'https://cuhacking.ca/', 'None', 'None', 'The organizers', 'None')];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
+(function getInputAndRunPlaywright() {
+    return __awaiter(this, void 0, void 0, function () {
+        var answers;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, inquirer_1.default.prompt([
+                        { type: 'input', name: 'EVENT_TITLE', message: 'Enter the event title:' },
+                        { type: 'input', name: 'EVENT_DATE', message: 'Enter the event date (YYYY/MM/DD):' },
+                        { type: 'input', name: 'EVENT_START_TIME', message: 'Enter the event start time (e.g., 10:00 AM):' },
+                        { type: 'input', name: 'EVENT_END_TIME', message: 'Enter the event end time (e.g., 12:00 PM):' },
+                        { type: 'input', name: 'EVENT_DESCRIPTION', message: 'Enter the event description:' },
+                        { type: 'input', name: 'EVENT_LOCATION', message: 'Enter the event location:' },
+                        { type: 'input', name: 'EVENT_EXPECTED_ATTENDEES', message: 'Enter the expected number of attendees:' },
+                        { type: 'input', name: 'ONLINE_PLATFORM', message: 'Enter the online platform used:' },
+                        { type: 'input', name: 'ONLINE_TOPICS', message: 'Enter the online topics discussed:' },
+                        { type: 'input', name: 'ONLINE_LOCATION', message: 'Enter the online broadcast origin location:' },
+                        { type: 'input', name: 'ONLINE_ORGANIZERS', message: 'Enter number of online organizers:' },
+                        { type: 'input', name: 'ONLINE_ORGANIZERS_ATTENDENCE_FROM_ORIGIN', message: 'Enter number of organizers attending from origin of broadcast:' },
+                        { type: 'input', name: 'RISK_SPEAKER_TOPICS', message: 'Enter speaker topics:' }
+                    ])];
+                case 1:
+                    answers = _a.sent();
+                    return [4 /*yield*/, (0, hybrid_1.scheduleHybrid)('Raef', 'Sarofiem', '101266475', 'raefsarofiem@cmail.carleton.ca', 'RAEF_PHONE_NUMBER', 'Ajaan', 'Nalliah', '101325463', 'ajaannalliah@cmail.carleton.ca', '6478773250', answers.EVENT_TITLE, answers.EVENT_DATE, answers.EVENT_START_TIME, answers.EVENT_END_TIME, answers.EVENT_DESCRIPTION, answers.EVENT_LOCATION, answers.EVENT_EXPECTED_ATTENDEES, answers.ONLINE_PLATFORM, answers.ONLINE_TOPICS, answers.ONLINE_LOCATION, answers.ONLINE_ORGANIZERS, answers.ONLINE_ORGANIZERS_ATTENDENCE_FROM_ORIGIN, answers.RISK_SPEAKER_TOPICS, 'Raef Sarofiem, Ajaan Nalliah', 'https://cuhacking.ca/', 'None', 'None', 'The organizers', 'None')];
+                case 2:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
     });
-}); })();
+})();
